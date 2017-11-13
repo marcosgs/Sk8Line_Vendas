05f528d774f9cad67794b29ff0bf3c6af2c9bf08
Marcos <>
2017-11-12 22:41:43 -0200

    Criação tela de login

---
+++ src/java/br/metodista/ead/loginMB.java
@@ -0,0 +1,43 @@
+package br.metodista.ead;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+public class loginMB {
+    private String usuario;
+    private String senha;
+    
+    private String usuarioSist ="root";
+    private String senhaSist = "root";
+    
+    public void setUsuario(String usuario){
+        this.usuario=usuario;
+    }
+    
+    public String getUsuario(){
+        return usuario;
+    }
+    
+    public void setSenha(String senha){
+        this.senha=senha;
+    }
+    
+    public String getSenha(){
+        return senha;
+    }
+    
+    public String validarLogon(){
+        
+        //if (this.usuario == this.usuarioSist && this.senha == this.senhaSist){ 
+        if (this.usuario.equals(usuarioSist) && this.senha.equals(senhaSist)){
+            FacesMessage msg = new FacesMessage("Usuário e senha são válidos!");
+            FacesContext.getCurrentInstance().addMessage("erro",msg);
+        }else{
+            FacesMessage msg = new FacesMessage("Usuário e senha são inválidos !");
+            FacesContext.getCurrentInstance().addMessage("erro",msg);
+        }
+        return null;
+    }
+}
